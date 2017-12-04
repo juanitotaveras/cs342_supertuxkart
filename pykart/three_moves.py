440beb61210b2ed261db993cf9c6bf53f4c96f54
Chris <chrisgramberg678@gmail.com>
2017-12-04 17:12:13 -0600

    tux shoots stuff now

--- pykart/three_moves.py
+++ pykart/three_moves.py
@@ -1,4 +1,4 @@
-#from pykart import Kart
+from pykart import Kart
 from time import sleep, time
 from random import random
 import tensorflow as tf
@@ -10,9 +10,9 @@ import util
 
 VALID_ACTIONS = 8
 STATES = 10
-LOAD_GRAPH_NAME = "three_moves_400_epochs.tfg"
-SAVE_GRAPH_NAME = "three_moves_400_epochs_2.tfg"
-TRAINING = True
+LOAD_GRAPH_NAME = "three_moves_400_epochs_3.tfg"
+SAVE_GRAPH_NAME = "three_moves_400_epochs_4.tfg"
+TRAINING = False
 PASS_PER_DATASET = 1
 EPOCHS = 400
 
@@ -59,7 +59,6 @@ class ImitationLearner():
 		self.valid_losses = []
 
 		self.in_frame = tf.placeholder(tf.float32, (None, 100,100,3), name='in_frame')
-		self.in_state = tf.placeholder(tf.float32, (None, STATES), name='in_state')
 		# expecting a one-hot vector
 		self.in_action = tf.placeholder(tf.uint8, (None, 3), name='in_action')
 
@@ -70,14 +69,10 @@ class ImitationLearner():
 		    h = tf.contrib.layers.conv2d(h, n, (3,3), stride=2, scope="conv%d"%i, weights_regularizer=tf.nn.l2_loss)
 		    print(h)
 		h = tf.contrib.layers.flatten(h)
-		print(h.shape, self.in_state.shape)
-		# h = tf.transpose(h) * self.in_state
 		print(h)
 		self.action_logit = tf.identity(tf.contrib.layers.fully_connected(h, 3, activation_fn=None), name='action_logit')
-		self.state_logit = tf.contrib.layers.fully_connected(h, STATES, activation_fn=None)
 		self.action_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.action_logit, labels=tf.cast(self.in_action, tf.float32))
-		self.state_loss = tf.losses.mean_squared_error(labels=self.in_state, predictions=self.state_logit)
-		self.total_loss = self.action_loss# + 1e-6 * tf.losses.get_regularization_loss()
+		self.total_loss = self.action_loss + 1e-6 * tf.losses.get_regularization_loss()
 		optimizer = tf.train.AdamOptimizer(.001, .9, .999)
 		self.opt = optimizer.minimize(self.total_loss)
  
@@ -87,7 +82,7 @@ class ImitationLearner():
 			self.sess.run(tf.global_variables_initializer())
 			self.initialize_vars = True
 		actions, states, frames = training_data
-		print(type(frames), frames.shape)
+		# print(type(frames), frames.shape)
 		for e in range(self.EPOCHS):
 			# do some training
 			for i in range(10):
@@ -113,12 +108,12 @@ def test_predict_action(sess,frame):
 	#tf.get_default_graph().get_tensor_by_name("in_frame:0")
 	graph = tf.get_default_graph()
 	PA = sess.run([graph.get_tensor_by_name("action_logit:0")], {graph.get_tensor_by_name("in_frame:0"):frame})[0][0]
-	print(PA)
+	# print(PA)
 	action = ''
 	for b in PA:
 		action+=str(int(b > 0))
 	r = int(action, 2)
-	print ("action: ", r)
+	# print ("action: ", r)
 	return r
 
 
@@ -136,80 +131,53 @@ if TRAINING:
 	#	np.insert(t_11,t_11.shape[0],t_12[i])
 	# il.train(parser(t_11),parser(t_11))
 
-	t0 = time()
-	K = Kart("lighthouse", 500, 500)
-	t1 = time()
-	K.restart()
-	t2 = time()
-	print( K.waitRunning() )
-	t3 = time()
-	print( K.running, t1-t0, t2-t1, t3-t2 )
-	sleep(1)
-
-	im, lbl = None, None
-	fwd, left, right = 1, 0, 0
-	# 4 = go forward
-	state, obs = K.step(4)
-	stuck_count = 0
-	for i in range(10000):
-		speed = state['speed']
-		if speed == 0:
-			if stuck_count == 10:
-				state, obs = K.step(64)
-				stuck_count = 0
-			else:
-				stuck_count+=1
-
-		state, obs = K.step(il.predict_action([skimage.transform.resize(obs, (100, 100))]))
-		# print( state )
-		from pylab import *
-		ion()
-		# figure()
-		# subplot(1,2,1)
-		if obs is not None:
-			if im is None:
-				im = imshow(obs)
-			else:
-				im.set_data(obs)
-		draw()
-		pause(0.001)
 else:
 	sess = tf.Session()
 	util.load(LOAD_GRAPH_NAME, session=sess)
-	t0 = time()
-	K = Kart("lighthouse", 500, 500)
-	t1 = time()
-	K.restart()
-	t2 = time()
-	print( K.waitRunning() )
-	t3 = time()
-	print( K.running, t1-t0, t2-t1, t3-t2 )
-	sleep(1)
 
-	im, lbl = None, None
-	fwd, left, right = 1, 0, 0
-	# 4 = go forward
-	state, obs = K.step(4)
-	stuck_count = 0
-	for i in range(10000):
-		speed = state['speed']
-		if speed == 0:
-			if stuck_count == 10:
-				state, obs = K.step(64)
-				stuck_count = 0
-			else:
-				stuck_count+=1
+# play the game
+t0 = time()
+K = Kart("lighthouse", 500, 500)
+t1 = time()
+K.restart()
+t2 = time()
+print( K.waitRunning() )
+t3 = time()
+print( K.running, t1-t0, t2-t1, t3-t2 )
+sleep(1)
 
-		state, obs = K.step(test_predict_action(sess, [skimage.transform.resize(obs, (100, 100))]))
-		# print( state )
-		from pylab import *
-		ion()
-		# figure()
-		# subplot(1,2,1)
-		if obs is not None:
-			if im is None:
-				im = imshow(obs)
-			else:
-				im.set_data(obs)
-		draw()
-		pause(0.001)
+im, lbl = None, None
+fwd, left, right = 1, 0, 0
+# 4 = go forward
+state, obs = K.step(4)
+stuck_count = 0
+file = open('actions.txt', 'w+')
+for i in range(10000):
+	speed = state['speed']
+	wrongway = state['wrongway']
+	if speed == 0 or wrongway > 0:
+		if stuck_count == 20:
+			# Then, each time you generate an action
+			file.write(str(64) + ':' + str(state['timestamp']) + '\n')
+			state, obs = K.step(64)
+			stuck_count = 0
+		else:
+			stuck_count+=1
+
+	n = 128 if np.random.rand() > .8 else 0
+	action = n + test_predict_action(sess, [skimage.transform.resize(obs, (100, 100))])
+	# Then, each time you generate an action
+	file.write(str(action) + ':' + str(state['timestamp']) + '\n')
+	state, obs = K.step(action)
+	print( action )
+	from pylab import *
+	ion()
+	# figure()
+	# subplot(1,2,1)
+	if obs is not None:
+		if im is None:
+			im = imshow(obs)
+		else:
+			im.set_data(obs)
+	draw()
+	pause(0.001)
